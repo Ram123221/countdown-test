c686a70690eb3ce02dd22ebafa98de6f995a45ec
RAM-GURUNG <ramgrg1024@gmail.com>
2023-04-30 14:14:53 +0545

    My countdown app

---
+++ countdown.js
@@ -0,0 +1,15 @@
+// const container = document.querySelector(".container");
+// const spans = document.querySelectorAll(".container span");
+// const rootEle =document.querySelector(":root");
+// window.addEventListener("resize", getHeight);
+// window.addEventListener("load", getHeight);
+// function getHeight(){
+//     let height = container.offsetHeight;
+//     console.log(height);
+//     height/=4;
+//     spans.forEach((span)=>{
+//         span.style.height = `${height}px`;
+//     });
+//     rootEle.style.setProperty("--offset",`-${height}px`);
+// }
+
